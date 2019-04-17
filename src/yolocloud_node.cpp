d64b6e37ff6762508bd988b2b5eaa44eb00bb64a
Rishi Shah <rishihahs@gmail.com>
2019-04-17 17:54:49 -0500

    Switch to villa darknet for inference

--- src/yolocloud_node.cpp
+++ src/yolocloud_node.cpp
@@ -5,15 +5,15 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <cv_bridge/cv_bridge.h>
-#include <tf/transform_listener.h>
-#include <tf_conversions/tf_eigen.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_eigen/tf2_eigen.h>
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <geometry_msgs/Point.h>
 
 #include <sensor_msgs/Image.h>
-#include <tmc_yolo2_ros/Detections.h>
-#include <tmc_yolo2_ros/Detection.h>
+#include <image_transport/image_transport.h>
+#include <image_transport/subscriber_filter.h>
 
 #include <knowledge_representation/LongTermMemoryConduit.h>
 #include <knowledge_representation/convenience.h>
@@ -24,29 +24,44 @@
 #include <villa_yolocloud/DetectedObject.h>
 #include <villa_yolocloud/GetShelfObjects.h>
 #include <villa_yolocloud/GetEntities.h>
+#include <villa_yolocloud/YoloModel.h>
+#include <opencv/highgui.h>
 
 #define VISUALIZE true
 
 typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image,
-                                                        sensor_msgs::Image,
-                                                        tmc_yolo2_ros::Detections> SyncPolicy;
+                                                        sensor_msgs::Image> SyncPolicy;
 
 
 class YoloCloudNode {
 private:
-    YoloCloud shelf_manager;
-    tf::TransformListener listener;
+    YoloCloud yoloCloud;
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener;
     std::unordered_map<int, int> entity_id_to_point;
     knowledge_rep::LongTermMemoryConduit ltmc; // Knowledge base
 
 
+#ifdef VISUALIZE
+    image_transport::Publisher viz_yolo_pub;
     ros::Publisher viz_pub;
+#endif
 
 public:
+    image_transport::ImageTransport it;
+    YoloModel model;
     bool received_first_message = false;
+
     YoloCloudNode(ros::NodeHandle node)
-        : ltmc(knowledge_rep::get_default_ltmc()) {
-        viz_pub = node.advertise<visualization_msgs::MarkerArray>("yoloobjects/markers", 1, true);
+        : tfListener(tfBuffer),
+          it(node),
+          ltmc(knowledge_rep::get_default_ltmc()) {
+
+#ifdef VISUALIZE
+        viz_yolo_pub = it.advertise("yolocloud/detections", 1);
+        viz_pub = node.advertise<visualization_msgs::MarkerArray>("yolocloud/markers", 1, true);
+#endif
+
         // NOTE: We assume there's only one YoloCloud, so this will blow away anything that is sensed
         ltmc.get_concept("sensed").remove_instances();
         ltmc.get_concept("scanned").remove_references();
@@ -58,13 +73,8 @@ public:
     }
 
     void data_callback(const sensor_msgs::Image::ConstPtr &rgb_image,
-                       const sensor_msgs::Image::ConstPtr &depth_image,
-                       const tmc_yolo2_ros::Detections::ConstPtr &yolo_detections) {
+                       const sensor_msgs::Image::ConstPtr &depth_image) {
         received_first_message = true;
-        if (yolo_detections->detections.empty()) {
-            return;
-        }
-
         cv_bridge::CvImagePtr cv_ptr;
         try {
             cv_ptr = cv_bridge::toCvCopy(rgb_image, sensor_msgs::image_encodings::BGR8);
@@ -73,30 +83,35 @@ public:
             return;
         }
 
-        tf::StampedTransform transform;
+        geometry_msgs::TransformStamped transform;
         try {
-            listener.waitForTransform("map", "head_rgbd_sensor_link", rgb_image->header.stamp, ros::Duration(0.1));
-            listener.lookupTransform("map", "head_rgbd_sensor_link", rgb_image->header.stamp, transform);
-        } catch (tf::TransformException &ex){
-            ROS_ERROR("%s",ex.what());
+            transform = tfBuffer.lookupTransform("map", "head_rgbd_sensor_link", rgb_image->header.stamp, ros::Duration(0.01));
+        } catch (tf2::TransformException &ex){
+            ROS_ERROR("%s", ex.what());
             return;
         }
 
-        Eigen::Affine3d camToMap;
-        tf::transformTFToEigen(transform, camToMap);
+        Eigen::Affine3d camToMap = tf2::transformToEigen(transform);
 
         cv::Mat depthI(depth_image->height, depth_image->width, CV_32FC1);
         memcpy(depthI.data, depth_image->data.data(), depth_image->data.size());
 
-        for (const tmc_yolo2_ros::Detection &detection : yolo_detections->detections) {
-            ImageBoundingBox bbox;
-            bbox.x = detection.x - detection.width/2;
-            bbox.y = detection.y - detection.height/2;
-            bbox.width = detection.width;
-            bbox.height = detection.height;
-            bbox.label = detection.class_name;
+        std::vector<Detection> dets = model.detect(&cv_ptr->image);
 
-            int idx = shelf_manager.addObject(bbox, cv_ptr->image, depthI, camToMap.cast<float>());
+        // Quit if there are no YOLO detections
+        if (dets.size() == 0) {
+            return;
+        }
+
+        for (const auto &detection : dets) {
+            ImageBoundingBox bbox;
+            bbox.x = detection.bbox.x;
+            bbox.y = detection.bbox.y;
+            bbox.width = detection.bbox.width;
+            bbox.height = detection.bbox.height;
+            bbox.label = detection.label;
+
+            int idx = yoloCloud.addObject(bbox, cv_ptr->image, depthI, camToMap.cast<float>());
 
             // If object was added to yolocloud, add to knowledge base
             if (idx >= 0) {
@@ -105,13 +120,19 @@ public:
         }
 
 #ifdef VISUALIZE
+        for (const auto &d : dets) {
+            cv::rectangle(cv_ptr->image, d.bbox, cv::Scalar(100, 100, 0), 3);
+        }
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", cv_ptr->image).toImageMsg();
+        viz_yolo_pub.publish(msg);
+
         visualize();
 #endif
     }
 
 
     void add_to_ltmc(int cloud_idx) {
-        std::string label = shelf_manager.labels.at(shelf_manager.objects->points[cloud_idx].label);
+        std::string label = yoloCloud.labels.at(yoloCloud.objects->points[cloud_idx].label);
         auto concept = ltmc.get_concept(label);
         auto entity = concept.create_instance();
         auto sensed = ltmc.get_concept("sensed");
@@ -123,7 +144,7 @@ public:
     bool get_entities(villa_yolocloud::GetEntities::Request &req, villa_yolocloud::GetEntities::Response &res) {
         std::vector<geometry_msgs::Point> map_locations;
         for (int eid : req.entity_ids) {
-            auto points = shelf_manager.objects->points;
+            auto points = yoloCloud.objects->points;
             geometry_msgs::Point p;
             // Requested an ID that's not in the cloud. Return NANs
             if (entity_id_to_point.count(eid) == 0) {
@@ -133,7 +154,7 @@ public:
                 p.y = NAN;
                 p.z = NAN;
             } else {
-                pcl::PointXYZL point = shelf_manager.objects->points[entity_id_to_point.at(eid)];
+                pcl::PointXYZL point = yoloCloud.objects->points[entity_id_to_point.at(eid)];
                 p.x = point.x;
                 p.y = point.y;
                 p.z = point.z;
@@ -148,28 +169,12 @@ public:
     }
 
 
-    bool get_shelf_objects(villa_yolocloud::GetShelfObjects::Request &req, villa_yolocloud::GetShelfObjects::Response &res) {
-        pcl::PointCloud<pcl::PointXYZL>::Ptr cloud = shelf_manager.sliceCloud(req.x_min, req.x_max, req.y_min, req.y_max, req.z_min, req.z_max);
-
-        for (const auto &point : cloud->points) {
-            villa_yolocloud::DetectedObject obj;
-            obj.x = point.x;
-            obj.y = point.y;
-            obj.z = point.z;
-            obj.label = shelf_manager.labels.at(point.label);
-            res.objects.push_back(obj);
-        }
-
-        return true;
-    }
-
-
     void visualize() {
         visualization_msgs::MarkerArray yolo_marker_array;
-        for (const auto &object : shelf_manager.objects->points) {
+        for (const auto &object : yoloCloud.objects->points) {
             visualization_msgs::Marker marker;
             marker.header.frame_id = "map";
-            marker.ns = shelf_manager.labels.at(object.label);
+            marker.ns = yoloCloud.labels.at(object.label);
             marker.id = 0;
             marker.type = 1;
             marker.action = 0;
@@ -191,10 +196,10 @@ public:
 
             visualization_msgs::Marker text_marker;
             text_marker.header.frame_id = "map";
-            text_marker.ns = shelf_manager.labels.at(object.label);
+            text_marker.ns = yoloCloud.labels.at(object.label);
             text_marker.id = 1;
             text_marker.type = 9;
-            text_marker.text = shelf_manager.labels.at(object.label);
+            text_marker.text = yoloCloud.labels.at(object.label);
             text_marker.action = 0;
             text_marker.pose.position.x = object.x;
             text_marker.pose.position.y = object.y;
@@ -225,13 +230,17 @@ int main (int argc, char **argv) {
     ros::NodeHandle n;
 
     YoloCloudNode yc_node(n);
+    yc_node.model.load("/home/bwilab/Desktop/train/darknet_data/config/model.names", "/home/bwilab/Desktop/train/darknet_data/config/model.cfg", "/home/bwilab/Desktop/train/darknet_data/backup/model.backup");
 
-    message_filters::Subscriber<sensor_msgs::Image> image_sub(n, "/hsrb/head_rgbd_sensor/rgb/image_rect_color", 10);
-    message_filters::Subscriber<sensor_msgs::Image> depth_sub(n, "/hsrb/head_rgbd_sensor/depth_registered/image", 10);
-    message_filters::Subscriber<tmc_yolo2_ros::Detections> yolo_sub(n, "/yolo2_node/detections", 10);
-    message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), image_sub, depth_sub, yolo_sub);
-    sync.registerCallback(boost::bind(&YoloCloudNode::data_callback, &yc_node, _1, _2, _3));
-
+    image_transport::SubscriberFilter image_sub(yc_node.it,
+                                                "/hsrb/head_rgbd_sensor/rgb/image_rect_color",
+                                                10,
+                                                image_transport::TransportHints("compressed"));
+    image_transport::SubscriberFilter depth_sub(yc_node.it,
+                                                "/hsrb/head_rgbd_sensor/depth_registered/image",
+                                                10);
+    message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), image_sub, depth_sub);
+    sync.registerCallback(boost::bind(&YoloCloudNode::data_callback, &yc_node, _1, _2));
 
     ROS_INFO("Started. Waiting for inputs.");
     while (ros::ok() && !yc_node.received_first_message) {
@@ -239,8 +248,6 @@ int main (int argc, char **argv) {
         ros::spinOnce();
     }
 
-    ros::ServiceServer getShelfObjects = n.advertiseService("getShelfObjects", &YoloCloudNode::get_shelf_objects,
-                                                            &yc_node);
     ros::ServiceServer getEntities = n.advertiseService("getEntities", &YoloCloudNode::get_entities, &yc_node);
 
     ros::spin();
