4561e68133978fbf6179c6577991cced34157225
Nick Walker <nswalker@cs.washington.edu>
2020-04-09 22:43:13 -0500

    Drop unused files
    Remove YOLO components until we can find a widely available dependency for this

--- src/BoundingBox.cpp
+++
@@ -1,9 +0,0 @@
-#include <BoundingBox.h>
-#include "Eigen/Dense"
-
-BoundingBox::BoundingBox(const Eigen::Vector4f &min, const Eigen::Vector4f &max, const Eigen::Vector4f &centroid, const Eigen::Quaternionf &orientation, const Eigen::Vector4f &position, const int index):
-			min(min), max(max),
-			centroid(centroid), position(position), orientation(orientation),
-			index(index){
-
-}
